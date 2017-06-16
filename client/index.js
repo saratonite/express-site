e28264e0e521614a1ff0fe830db89a67ec640b12
Sarath <sarath.hacks@gmail.com>
2017-06-16 19:50:34 +0530

    Bootstraped react app :smile:

--- client/index.js
+++ client/index.js
@@ -1,2 +1,12 @@
 console.log('Hello Worldss');
 import './index.scss';
+
+import React from 'react';
+import { render } from 'react-dom';
+
+var appEl = document.getElementById('app');
+if(appEl) {
+
+    render(<p>Welcome to react app</p>,appEl);
+
+}
