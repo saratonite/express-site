dae35c4928ddab818ddc822f2e4aa4ea589080fb
Sarath <sarath.hacks@gmail.com>
2017-06-29 18:49:10 +0530

    Updated Signup and Login component

--- client/components/pages/Login.js
+++ client/components/pages/Login.js
@@ -2,10 +2,44 @@ import React , { Component } from 'react';
 
 export default class Login extends Component {
 
+    constructor(props) {
+
+        super(props);
+
+        this.state = {email:'',password:''};
+
+        this.handleInputOnChange = this.handleInputOnChange.bind(this);
+
+    }
+
+    handleInputOnChange(e) {
+        this.setState({[e.target.name]:e.target.value});
+
+    }
+
     render() {
+        
         return(
-            <div className="card card-outline-primary mb-3 text-center">
-               <h1>Login </h1>
+            <div className="container">
+               <form className="form-signin">
+                    <h2 className="form-signin-heading">Please sign in</h2>
+
+                    <div className="form-group">
+                        <label htmlFor="inputEmail" className="sr-only">Email address</label>
+                        <input type="email" name="email" id="inputEmail" value={this.state.email} onChange={this.handleInputOnChange} className="form-control" placeholder="Email address" required autoFocus />
+                    </div>
+                    <div className="form-group">
+                         <label htmlFor="inputPassword" className="sr-only">Password</label>
+                         <input type="password" name="password" id="inputPassword" value={this.state.password} onChange={this.handleInputOnChange} className="form-control" placeholder="Password" required />
+                    </div>
+                   
+                    <div className="checkbox">
+                        <label>
+                            <input type="checkbox" value="remember-me" /> Remember me
+                        </label>
+                    </div>
+                    <button className="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+                </form>
             </div>
         )
     }
--- client/components/pages/Signup.js
+++ client/components/pages/Signup.js
@@ -1,11 +1,44 @@
 import React , { Component } from 'react';
 
 export default class Signup extends Component {
+    constructor(props) {
 
+        super(props);
+
+        this.state = {email:'',password:'',confirmPassword:'',remember:''};
+
+        this.handleInputOnChange = this.handleInputOnChange.bind(this);
+
+    }
+
+    handleInputOnChange(e) {
+        console.log(e)
+        this.setState({[e.target.name]:e.target.value});
+
+    }
     render() {
         return(
-            <div className="card card-outline-primary mb-3 text-center">
-               <h1>Signup </h1>
+            <div className="container">
+               <form className="form-signin">
+                    <h2 className="form-signin-heading">Sign up</h2>
+                    <div className="form-group">
+                        <label htmlFor="inputEmail" className="sr-only">Email address</label>
+                        <input type="email"  name="email" onChange={ this.handleInputOnChange } id="inputEmail" className="form-control" placeholder="Email address" required autoFocus />
+                    </div>
+                     <div className="form-group">
+                        <label htmlFor="inputPassword" className="sr-only">Password</label>
+                        <input type="password" name="password" onChange={ this.handleInputOnChange } id="inputPassword" className="form-control" placeholder="Password" required />
+                    </div>
+                     <div className="form-group">
+                        <input type="password" name="confirmPassword" onChange={ this.handleInputOnChange } id="inputPassword" className="form-control" placeholder="Confirm Password" required />
+                    </div>
+                    <div className="checkbox">
+                    <label>
+                        <input type="checkbox" name="remember" onChange={this.handleInputOnChange}  /> I agree
+                    </label>
+                    </div>
+                    <button className="btn btn-lg btn-primary btn-block" type="submit">Start</button>
+                </form>
             </div>
         )
     }
