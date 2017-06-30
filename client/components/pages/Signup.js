398410b01e5a01e6401b8b58680c72b7bdc805c4
Sarath <sarath.hacks@gmail.com>
2017-06-30 17:18:40 +0530

    Signup page validations

--- client/components/pages/Signup.js
+++ client/components/pages/Signup.js
@@ -1,13 +1,20 @@
 import React , { Component } from 'react';
+import Validator from 'validator';
+import classNames from 'classnames';
 
+// TODO: Connect to redux store
+
+ 
 export default class Signup extends Component {
     constructor(props) {
 
         super(props);
 
-        this.state = {email:'',password:'',confirmPassword:'',remember:''};
+        this.state = {email:'',password:'',confirmPassword:'',remember:'', errors:{}};
 
         this.handleInputOnChange = this.handleInputOnChange.bind(this);
+
+        this.handleFormSubmit = this.handleFormSubmit.bind(this);
 
     }
 
@@ -16,21 +23,71 @@ export default class Signup extends Component {
         this.setState({[e.target.name]:e.target.value});
 
     }
+
+    // TODO: Signup request
+
+    handleFormSubmit(e) {
+
+        this.validate();
+
+        e.preventDefault();
+
+    }
+
+    // TODO: Form validations
+
+    validate() {
+
+        let errors = {};
+        if(Validator.isEmpty(this.state.email)) {
+
+             errors.email = "This field required";
+        }
+
+        if(!Validator.isEmail(this.state.email) && !errors.email) {
+
+            errors.email = "Please provide a valid email";
+        }
+
+        if(Validator.isEmpty(this.state.password)) {
+
+            errors.password = 'Password field required';
+        }
+
+        if((Validator.isEmpty(this.state.confirmPassword)|| !Validator.equals(this.state.password,this.state.confirmPassword)) && !errors.password) {
+
+            errors.confirmPassword ='Please confirm your password'
+
+        }
+    
+        this.setState({errors:errors});
+
+    }
+
     render() {
+
+        let { errors }= this.state;
+
+
+        
         return(
             <div className="container">
-               <form className="form-signin">
+               <form className="form-signin" onSubmit={this.handleFormSubmit}>
                     <h2 className="form-signin-heading">Sign up</h2>
-                    <div className="form-group">
+                    <div className={classNames("form-group",{'has-danger':errors.email}) }>
                         <label htmlFor="inputEmail" className="sr-only">Email address</label>
-                        <input type="email"  name="email" onChange={ this.handleInputOnChange } id="inputEmail" className="form-control" placeholder="Email address" required autoFocus />
+                        <input   name="email" onChange={ this.handleInputOnChange } id="inputEmail" className="form-control" placeholder="Email address" autoFocus />
+                        { errors.email && <div className="form-control-feedback">{errors.email}</div> }
+
                     </div>
-                     <div className="form-group">
+                     <div className={classNames("form-group",{'has-danger':errors.password}) }>
                         <label htmlFor="inputPassword" className="sr-only">Password</label>
-                        <input type="password" name="password" onChange={ this.handleInputOnChange } id="inputPassword" className="form-control" placeholder="Password" required />
+                        <input type="password" name="password" onChange={ this.handleInputOnChange } id="inputPassword" className="form-control" placeholder="Password" />
+                         { errors.password && <div className="form-control-feedback">{errors.password}</div> }
                     </div>
-                     <div className="form-group">
-                        <input type="password" name="confirmPassword" onChange={ this.handleInputOnChange } id="inputPassword" className="form-control" placeholder="Confirm Password" required />
+                     <div className={classNames("form-group",{'has-danger':errors.confirmPassword}) }>
+                        <input type="password" name="confirmPassword" onChange={ this.handleInputOnChange } id="inputPassword" className="form-control" placeholder="Confirm Password" />
+                          { errors.confirmPassword && <div className="form-control-feedback">{errors.confirmPassword}</div> }
                     </div>
                     <div className="checkbox">
                     <label>
