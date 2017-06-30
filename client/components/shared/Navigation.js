bbe5155654f230df81a5fbd393b51d137efb1081
Sarath <sarath.hacks@gmail.com>
2017-06-30 15:56:41 +0530

    Updated Navigation component apperaence

--- client/components/shared/Navigation.js
+++ client/components/shared/Navigation.js
@@ -9,11 +9,11 @@ export default class Navigation extends Component {
 
     render() {
         return(
-            <ul className="nav nav-pills">
-                <li role="presentation"><Link to="/">Home</Link></li>
-                <li role="presentation"><Link to="/about">About</Link></li>
-                <li role="presentation"><Link to="/signup">Signup</Link></li>
-                <li role="presentation"><Link to="/login">Login</Link></li>
+            <ul className="nav nav-pills justify-content-end">
+                <li className="nav-item" role="presentation"><Link className="nav-link" to="/">Home</Link></li>
+                <li className="nav-item" role="presentation"><Link className="nav-link" to="/about">About</Link></li>
+                <li className="nav-item" role="presentation"><Link className="nav-link" to="/signup">Signup</Link></li>
+                <li className="nav-item" role="presentation"><Link className="nav-link" to="/login">Login</Link></li>
             </ul>
         )
     }
